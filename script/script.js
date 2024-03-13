45d557cf12360d03280c5467257b62f5002020df
Hesam Odin Abbaspour <h.abaspour719@gmail.com>
2024-03-13 15:01:34 +0330

    Upload Script Of .Shopper Project

--- script/script.js
+++ script/script.js
@@ -9,24 +9,24 @@
     let today = new Date(),
         dd = String(today.getDate()).padStart(2, "0"),
         mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
+        yyyy = 2025,
         nextYear = yyyy + 1,
-        dayMonth = "09/30/",
-        birthday = dayMonth + yyyy;
+        dayMonth = "12/31/",
+        event = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
+    if (today > event) {
       birthday = dayMonth + nextYear;
     }
     //end
     
-    const countDown = new Date(birthday).getTime(),
+    const countDown = new Date(event).getTime(),
         x = setInterval(function() {    
   
           const now = new Date().getTime(),
                 distance = countDown - now;
   
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
             document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
@@ -52,3 +52,163 @@
       });
     });
     
+
+let slider= document.getElementById("slider")
+let slide1= document.getElementById("slide1")
+let slide2= document.getElementById("slide2")
+let slide3= document.getElementById("slide3")
+let slide4= document.getElementById("slide4")
+let i =0
+setInterval(()=>{
+    let body_width = document.body.clientWidth
+    if(body_width < 730){
+        document.getElementById("dot1").addEventListener("click", function(){
+            if(i==(-300)){
+                i= -400
+                slide3.style.transform= "translatex(400%)"
+            }else if(i==300){
+                i= 400
+                slide1.style.transform= "translatex(-400%)"
+            }else {
+                i=0
+                slide1.style.transform= "translatex(0)"
+                slide2.style.transform= "translatex(0)"
+                slide3.style.transform= "translatex(0)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+        document.getElementById("dot2").addEventListener("click", function(){
+            if(i==-300){
+                i= -100
+                slide1.style.transform= "translatex(400%)"
+                slide2.style.transform= "translatex(0)"
+            }
+            else if(i==200){
+                i=300
+                slide2.style.transform= "translatex(-400%)"
+            }else if(i==400){
+                i=300
+            }else {
+                i= -100
+                slide2.style.transform= "translatex(0)"
+                slide3.style.transform= "translatex(0)"
+                slide4.style.transform= "translatex(0)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+        document.getElementById("dot3").addEventListener("click", function(){
+            if(i==100){
+                i= 200
+                slide3.style.transform= "translatex(-400%)"
+            }else if(i==300){
+                i=200
+                slide1.style.transform= "translatex(0)"
+            }
+            else{
+                i= -200
+                slide1.style.transform= "translatex(400%)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+        document.getElementById("dot4").addEventListener("click", function(){
+            if(i==0){
+                i= 100
+                slide4.style.transform= "translatex(-400%)"
+            }else if(i==200){
+                i=33.33
+                slide2.style.transform= "translatex(0)"
+            }else if(i==400){
+                i= -300
+                slide4.style.transform= "translatex(0)"
+                slide1.style.transform= "translatex(400%)"
+                slide2.style.transform= "translatex(400%)"
+                slide3.style.transform= "translatex(0)"
+            }
+            else {
+                i= -300
+                slide1.style.transform= "translatex(400%)"
+                slide2.style.transform= "translatex(400%)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+    }else {
+        document.getElementById("dot1").addEventListener("click", function(){
+            if(i==(-100)){
+                i= -133.33
+                slide3.style.transform= "translatex(400%)"
+            }else if(i==100){
+                i= 133.33
+                slide1.style.transform= "translatex(-400%)"
+            }else {
+                i=0
+                slide1.style.transform= "translatex(0)"
+                slide2.style.transform= "translatex(0)"
+                slide3.style.transform= "translatex(0)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+            console.log(i)
+        })
+        document.getElementById("dot2").addEventListener("click", function(){
+            if(i==-100){
+                i= -33.33
+                slide1.style.transform= "translatex(400%)"
+                slide2.style.transform= "translatex(0)"
+            }
+            else if(i==66.67){
+                i=100
+                slide2.style.transform= "translatex(-400%)"
+            }else if(i==133.33){
+                i=100
+            }else {
+                i= -33.33
+                slide2.style.transform= "translatex(0)"
+                slide3.style.transform= "translatex(0)"
+                slide4.style.transform= "translatex(0)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+        document.getElementById("dot3").addEventListener("click", function(){
+            if(i==33.33){
+                i= 66.67
+                slide3.style.transform= "translatex(-400%)"
+            }else if(i==100){
+                i=66.67
+                slide1.style.transform= "translatex(0)"
+            }
+            else{
+                i= -66.67
+                slide1.style.transform= "translatex(400%)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+        document.getElementById("dot4").addEventListener("click", function(){
+            if(i==0){
+                i= 33.33
+                slide4.style.transform= "translatex(-400%)"
+            }else if(i==66.67){
+                i= 33.33
+                slide2.style.transform= "translatex(0)"
+            }else{
+                i= -100
+                slide4.style.transform= "translatex(0)"
+                slide1.style.transform= "translatex(400%)"
+                slide2.style.transform= "translatex(400%)"
+                slide3.style.transform= "translatex(0)"
+            }
+            slider.style.transform= "translatex("+ i +"%)"
+        })
+    
+    }
+},100)
+let dots= document.querySelectorAll(".dots")
+dots.forEach(function(dot){
+    dot.addEventListener("click", ()=>{
+        removeDot()
+        dot.classList.add("red-dot")
+    })
+})
+function removeDot() {
+    dots.forEach((dot)=>{
+        dot.classList.remove("red-dot")
+    })
+}
